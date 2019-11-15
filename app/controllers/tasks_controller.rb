b5ae9f06fe1dc99880e7565771d6557772a851c5
Reid Shea <reidshea3@gmail.com>
2019-11-15 11:04:49 +0100

    Tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to show_path(@task)
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit('title', 'details')
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  get 'task/:id', to: 'tasks#show', as: :show
+
+  get 'new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create', as: :create
 end
